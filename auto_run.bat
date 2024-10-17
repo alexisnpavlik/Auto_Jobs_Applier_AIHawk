7985194a504e56554d85dea85fc65e672b7e2391
Alexis Medina <alexisnpavlik@gmail.com>
2024-10-17 17:17:08 -0500

    Create auto_run.bat

---
+++ auto_run.bat
@@ -0,0 +1,3 @@
+cd "C:\Users\alexi\OneDrive\Documents\GitHub\Auto_Jobs_Applier_AIHawk"
+
+C:\Users\alexi\anaconda3\envs\autoai\python.exe C:\Users\alexi\OneDrive\Documents\GitHub\Auto_Jobs_Applier_AIHawk\main.py
